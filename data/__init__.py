0c46bfa0bcd7f1b4268b2eb76e36f83dfbeeb782
snim2 <mount.sarah@gmail.com>
2013-11-24 11:57:30 +0000

    Added functions for some basic UK postcode manipulation

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,10 @@
 import csv
 import json
+import requests
+
+from math import radians, cos, sin, asin, sqrt
+
+_PC_FINDER = 'http://uk-postcodes.com/'
 
 _UNI_FILE = 'data/uk-uni-locations.csv'
 
@@ -20,3 +25,36 @@ def load_uni_data():
                                       'long'     : row[3]
                                     }
     return universities, uni_locations
+
+
+def postcode_to_long_lat(pc):
+    """TODO: Sort out exception handling
+    """
+    postcode = pc.replace(' ', '')
+    url = _PC_FINDER + '/postcode/' + postcode + '.' + fmt
+    r = requests.get(url)
+    if r.status_code != 200: return 0, 0
+    content = r.content
+    result = {}
+    result['lat'] = content['geo']['lat']
+    result['lang'] = content['geo']['lng']
+    return result 
+
+
+def distance_between_postcodes(pc1, pc2):
+    # Grab data for pc1
+    postcode1 = pc1.replace(' ', '')
+    url = _PC_FINDER + '/postcode/' + postcode1 + '.json'
+    r1 = requests.get(url)
+    if r1.status_code != 200: return 0
+    content1 = r1.content
+    # Grab data for pc2
+    postcode2 = pc2.replace(' ', '')
+    url = _PC_FINDER + '/postcode/' + postcode2 + '.json'
+    r2 = requests.get(url)
+    if r2.status_code != 200: return 0
+    content2 = r2.content
+    # Get distance
+    e1, n1 = content1['geo']['easting'], content1['geo']['northing']
+    e2, n2 = content2['geo']['easting'], content2['geo']['northing']
+    return math.sqrt(pow((e2 - e1), 2) + pow((n2 - n1), 2))
