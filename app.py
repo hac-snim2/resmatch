ee03ab85ec8276282fe81054fc5ffe2b1366ac2e
snim2 <mount.sarah@gmail.com>
2013-11-24 13:45:15 +0000

    Now using CSV file with postcode data rather than making a RESTful call. App no longer times out when testing with foreman

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+import difflib
 import os
 from flask import * # TODO: Specify imports
 
 import data # Load data from CSV files in this repo
 
-# Load data from CSV files
-universities, uni_locations = data.load_uni_data()
 
+# Load data from CSV and JSON files
+universities, uni_locations = data.load_uni_data()
+orgs = data.load_orgs_data()
+postcodes = data.load_postcode_data()
+
+
+# Set up application.
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.secret_key = os.urandom(24) # Needed for sessions.
@@ -14,10 +20,11 @@ app.secret_key = os.urandom(24) # Needed for sessions.
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        flash('Searching for companies nere %s. Please be patient....' % request.form['university'])
-        session['university'] = request.form['university']
+        best_guess = difflib.get_close_matches(request.form['university'], universities)[0]
+        flash('Searching for companies nere %s. Please be patient....' % best_guess)
+        session['university'] = best_guess
         return redirect(url_for('results'))
-    return '''
+    return """
 <html>
   <head></head>
   <body>
@@ -27,8 +34,35 @@ def index():
         </form>
   </body>
 </html>
-    '''
+    """
+
 
 @app.route('/results/')
 def results():
-    return 'Hello world!\n'
+    try:
+        uni = session['university']
+    except KeyError:
+        return """<!DOCTYPE html>
+<html>
+<head><title>No university specified in search box</title></head>
+<body>
+<h1>You didn't give me a University to search for!</h1>
+<p>Please go <a href="/">back</a> and try again.</p>
+</body>
+</html>
+"""
+    print 'DEBUG %s' % uni
+    postcode = uni_locations[uni]['postcode']
+    nearby = data.find_nearby_orgs(postcode, orgs, postcodes)
+    for key in nearby: print nearby[key] + '\n\n'
+    results = {}
+    #    results['university'] = uni
+#    results['long'] = uni_locations[uni]['long']
+#    results['lat'] = uni_locations[uni]['lat']
+#    results['postcode'] = uni_locations[uni]['postcode']
+#    companies = []
+#    for co in nearby:
+#        org = {}
+#        org['name'] = co[
+#    return 
+    return 'You are searching for companies around %s\n' % session['university']
--- data/__init__.py
+++ data/__init__.py
@@ -4,22 +4,24 @@ import requests
 
 from math import sqrt
 
-_PC_FINDER = 'http://uk-postcodes.com/'
-
 _UNI_FILE = 'data/uk-uni-locations.csv'
 
 _ORGS_FILE = 'data/orgs.json'
 
+_PC_FILE = 'data/postcodes.csv'
+
 MIN_DIST = 50
 
 
-def find_nearby_orgs(postcode, orgs):
+def find_nearby_orgs(postcode, orgs, postcodes):
     """TODO: Maybe better to hold the orgs data here?
     """
     nearby = {}
     origin = postcode.replace(' ', '')
     for pc in orgs.keys():
-        if distance_between_postcodes(origin, pc) < MIN_DIST:
+        dist = distance_between_postcodes(origin, pc, postcodes) 
+        if dist is None: continue
+        if dist < MIN_DIST:
             nearby[pc] = orgs[pc]
     return nearby
 
@@ -31,9 +33,29 @@ def load_orgs_data():
     return dict()
 
 
+def load_postcode_data():
+    """Load CSV file of postcode data into memory.
+    Return in JSON.
+    """
+    postcodes = dict()
+    with open(_PC_FILE, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0] == 'postcode' or len(row) != 9: continue
+            postcodes[row[0].replace(' ', '')] = {'postcode' : row[0],
+                                                  'eastings' : row[1],
+                                                  'northings' : row[2],
+                                                  'latitude' : row[3],
+                                                  'longitude' : row[4],
+                                                  'town' : row[5],
+                                                  'region' : row[6]
+                                                  }
+    return postcodes
+
+
 def load_uni_data():
     """Load CSV file of university locations into memory.
-    Return in JSON.
+    Return a dict.
     """
     universities = []
     uni_locations = {}
@@ -49,36 +71,43 @@ def load_uni_data():
     return universities, uni_locations
 
 
-def postcode_to_long_lat(pc):
-    """TODO: Sort out exception handling
-    """
-    postcode = pc.replace(' ', '')
-    url = _PC_FINDER + '/postcode/' + postcode + '.' + fmt
-    r = requests.get(url)
-    if r.status_code != 200: return 0, 0
-    content = r.content
-    result = {}
-    result['lat'] = content['geo']['lat']
-    result['lang'] = content['geo']['lng']
-    return result 
+# def postcode_to_long_lat(pc):
+#     """TODO: Sort out exception handling
+#     """
+#     postcode = pc.replace(' ', '')
+#     url = _PC_FINDER + '/postcode/' + postcode + '.' + fmt
+#     r = requests.get(url)
+#     if r.status_code != 200: return 0, 0
+#     content = r.content
+#     result = {}
+#     result['lat'] = content['geo']['lat']
+#     result['lang'] = content['geo']['lng']
+#     return result 
 
 
-def distance_between_postcodes(pc1, pc2):
+def distance_between_postcodes(pc1, pc2, postcodes):
     # Grab data for pc1
-    postcode1 = pc1.replace(' ', '')
-    url = _PC_FINDER + '/postcode/' + postcode1 + '.json'
-    r1 = requests.get(url)
-    if r1.status_code != 200: return 0
-    content1 = json.loads(r1.content)
-    # Grab data for pc2
-    postcode2 = pc2.replace(' ', '')
-    url = _PC_FINDER + '/postcode/' + postcode2 + '.json'
-    r2 = requests.get(url)
-    if r2.status_code != 200: return 0
-    content2 = json.loads(r2.content)
-    # Get distance
-    e1 = int(content1['geo']['easting'])
-    n1 = int(content1['geo']['northing'])
-    e2 = int(content2['geo']['easting'])
-    n2 = int(content2['geo']['northing'])
-    return sqrt(pow((e2 - e1), 2) + pow((n2 - n1), 2))
+    # postcode1 = pc1.replace(' ', '')
+    # url = _PC_FINDER + '/postcode/' + postcode1 + '.json'
+    # r1 = requests.get(url)
+    # if r1.status_code != 200: return 0
+    # content1 = json.loads(r1.content)
+    # # Grab data for pc2
+    # postcode2 = pc2.replace(' ', '')
+    # url = _PC_FINDER + '/postcode/' + postcode2 + '.json'
+    # r2 = requests.get(url)
+    # if r2.status_code != 200: return 0
+    # content2 = json.loads(r2.content)
+    # # Get distance
+    # e1 = int(content1['geo']['easting'])
+    # n1 = int(content1['geo']['northing'])
+    # e2 = int(content2['geo']['easting'])
+    # n2 = int(content2['geo']['northing'])
+    try:
+        e1 = postcodes[pc1.replace(' ', '')]['eastings']
+        n1 = postcodes[pc1.replace(' ', '')]['northings']
+        e2 = postcodes[pc2.replace(' ', '')]['eastings']
+        n2 = postcodes[pc2.replace(' ', '')]['northings']
+        return sqrt(pow((e2 - e1), 2) + pow((n2 - n1), 2))
+    except KeyError:
+        return None
